c8def03d78c7155b31502a2905990a80421756af
otter <justineabanilla80@gmail.com>
2025-02-03 10:36:54 +0800

    Add files via upload

---
+++ MYSPACE/song.js
@@ -0,0 +1,81 @@
+export const songList = [
+    {
+        img: 'https://i.ytimg.com/vi/tGv7CUutzqU/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBy9W5UGToDTJUT9ajkBRpQujnp_g',
+        name: 'About You',
+        music: 'https://www.dropbox.com/scl/fi/ei11wyryi6rmujz4f2sa4/About-You.mp3?rlkey=obcbvzrdbrpidknnecxqiteb0&e=2&st=kjsfdg3x&raw=1',
+        artist:"The 1975"
+    },
+    {
+        img: 'https://i1.sndcdn.com/artworks-I1C8ylgQ0LPk0qhV-44PbnA-t500x500.jpg',
+        name: 'Its Not Living',
+        music: 'https://www.dropbox.com/scl/fi/vppxtpu0nbtukkxnexbwn/Its-Not-Living-If-Its-Not-With-You.mp3?rlkey=gr443iszerb9eq4zs20mv9rcv&st=woe43t3p&raw=1',
+        artist:"The 1975"
+    },
+    {
+        img: 'https://i.scdn.co/image/ab67616d0000b27304f21e12e67ba0dfd5db4fbf',
+        name: 'Robbers',
+        music: 'https://www.dropbox.com/scl/fi/5yu5te8cl5a8kxbjyysun/Robbers.mp3?rlkey=0g58fyy90nu80mcm9tb2xny18&st=9ihbq2vt&raw=1',
+        artist:"The 1975"
+    }
+    ,
+    {
+        img: 'https://i.ytimg.com/vi/Rht8rS4cR1s/maxresdefault.jpg',
+        name: 'Multo',
+        music: 'https://www.dropbox.com/scl/fi/g2jnlpj8acirfgyq0j9bj/multo.mp3?rlkey=3k0i60vvbrzhqw71q7s7bz2ik&st=bdjgezz4&raw=1',
+        artist:"Cup of joe" 
+    }
+    ,
+    {
+        img: 'https://i.scdn.co/image/ab67616d0000b27326781df2358ea80f507dc52d',
+        name: 'unti-unti',
+        music: 'https://www.dropbox.com/scl/fi/svpmzeeadp1zta8tqj353/untiunti.mp3?rlkey=pab3mvpb2lmgnnievvwb1rjgi&st=8wr307q4&raw=1',
+        artist:"Up Dharma Down"
+    }
+    ,
+    {
+        img: 'https://i.ytimg.com/vi/NTmDN-hejDY/maxresdefault.jpg',
+        name: 'Wag Na Lang',
+        music: 'https://www.dropbox.com/scl/fi/xhng11rjrr3yq6dynv0am/wagnalang.mp3?rlkey=co0bpkro9d4nvrginobrevpi8&st=sxgu64sy&raw=1',
+        artist:"Cup of joe"
+    }
+    ,
+    {
+        img: 'https://i.ytimg.com/vi/Dlz_XHeUUis/maxresdefault.jpg',
+        name: 'White Ferrari',
+        music: 'https://www.dropbox.com/scl/fi/a23gb9de9hbasa125o2mb/whiteferrari.mp3?rlkey=w5f632u1kx6ym3b2z40ta1e74&st=mmhtavn9&raw=1',
+        artist: "Frank Ocean"
+    },
+    {
+        img: 'https://i.pinimg.com/736x/76/46/95/764695fbb81cbc573ee4677fdb81daed.jpg',
+        name: 'Backburner',
+        music: 'https://www.dropbox.com/scl/fi/4f918b79gliv5anj5poks/backburner.mp3?rlkey=lf6fqmycb5wte623iqupmp37i&st=d6wtjwqy&raw=1',
+        artist: "NIKI"
+    },
+   
+    {
+        img: 'https://i.scdn.co/image/ab67616d0000b2732c2861964a03f27e3093f1b2',
+        name: 'Autumn',
+        music: 'https://www.dropbox.com/scl/fi/jm7ph6o6fbwfqe9lxwfsw/autumn.mp3?rlkey=xhni5kx266u2t13u24xsji4tm&st=ryz4q3e3&raw=1',
+        artist: "NIKI"
+    },
+    {
+        img: 'https://i.ytimg.com/vi/q0880Uog8gg/sddefault.jpg?v=6245c099',
+        name: 'Mananatili',
+        music: 'https://www.dropbox.com/scl/fi/0i37nme3kc2boqcuiw5d2/mananatili.mp3?rlkey=yuogobz9708cfymuk1knxm7vh&st=sk0qvdod&raw=1',
+        artist: "Cup of Joe"
+    },
+    {
+        img: 'https://i.ytimg.com/vi/7VKs9MSAit4/sddefault.jpg',
+        name: 'Blue Sky',
+        music: 'https://www.dropbox.com/scl/fi/u0oajqwl1g4u1i3eakhrv/bluesky.mp3?rlkey=eroqmk9katnjxurm4jvev15ez&st=qdtzqaz6&raw=1',
+        artist: "Hale"
+    },
+    {
+     img: 'https://i.ytimg.com/vi/q3zqJs7JUCQ/maxresdefault.jpg',
+     name: 'Fortnight',
+     music: 'https://www.dropbox.com/scl/fi/hrqrl8rlssf12k2h80xk4/fortnight.mp3?rlkey=ie1qn9ef0gof0hj3zln44soy5&st=c99p7cfv&raw=1',
+     artist: "Taylor Swift"
+    
+ }
+    
+ ]
